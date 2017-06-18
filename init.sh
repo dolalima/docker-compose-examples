0f4cb8a86d7c5693700bd85ecec52ba6b3d03111
Dola <dola@lti.net.br>
2017-06-18 10:37:27 -0300

    docker compose wordpress

---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker swarm init
+
+docker service create
+--name portainer \
+--publish 9000:9000 \
+--constraint 'node.role == manager' \
+--mount type=bind,src=//var/run/docker.sock,dst=/var/run/docker.sock \
+portainer/portainer \
+-H unix:///var/run/docker.sock
+
+
+
